fb97e848d566bb3b2a96462cd3155cd8ded5069b
Marvin Steadfast <marvin@xsteadfastx.org>
2016-06-09 14:44:01 +0000

    mod_api: view import in one line

--- app/mod_api/__init__.py
+++ app/mod_api/__init__.py
@@ -4,5 +4,4 @@ from flask import Blueprint
 mod_api = Blueprint('mod_api', __name__, url_prefix='/api')
 
 
-from app.mod_api import errors
-from app.mod_api import views
+from app.mod_api import errors, views
